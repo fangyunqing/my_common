02767fd996f844011b2895ae9c31d2deed465de9
fyq <245960548@qq.com>
2023-03-19 22:27:07 +0800

    初始化

---
+++ original/util.py
@@ -0,0 +1,21 @@
+import ctypes
+
+
+def unsigned_right_shift(n, i):
+    def int_overflow(val):
+        maxint = 2147483647
+        if not -maxint - 1 <= val <= maxint:
+            val = (val + (maxint + 1)) % (2 * (maxint + 1)) - maxint - 1
+        return val
+
+    # 数字小于0，则转为32位无符号uint
+    if n < 0:
+        n = ctypes.c_uint32(n).value
+    # 正常位移位数是为正数，但是为了兼容js之类的，负数就右移变成左移好了
+    if i < 0:
+        return -int_overflow(n << abs(i))
+    return int_overflow(n >> i)
+
+
+if __name__ == "__main__":
+    print(unsigned_right_shift(-1000, 8))
